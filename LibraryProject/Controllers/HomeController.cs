11eb3d1359e958a8c4b91babcaa956362cc1d92d
ozan <ozan@SKYNET>
2017-09-28 22:48:07 +0300

    Add project files.

---
+++ LibraryProject/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryProject.Controllers
+{
+
+    using Models;
+
+    public class HomeController : Controller
+    {
+        LIBRARYEntities2 db = new LIBRARYEntities2();
+
+
+        public ActionResult Index()
+        {
+
+            return View();
+ 
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            
+            Books model = new Books();
+            Publishers model1 = new Publishers();
+
+            model.BookName = form["bookName"];
+            model.Writer = form["writer"];
+            model1.PublisherName = form["publisher"];
+
+            model.PublishDate=Convert.ToDateTime( form["publishDate"]);
+
+           
+          
+
+            db.Books.Add(model);
+            db.Publishers.Add(model1);
+            db.SaveChanges();
+
+            return View();
+
+        }
+
+        public ActionResult Create()
+        {
+
+   
+            return View();
+
+        }
+          
+
+       
+
+        public ActionResult List()
+        {
+            
+            return View(db.Books.ToList());
+
+        }
+
+
+  
+
+
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
